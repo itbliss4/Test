add1a882cbf7ddc618f276c27328e58cd9cce3d1
abcd <abcd@bliss-its-mac-pro.local>
2017-05-09 13:07:55 -0700

    Add button and one line of code

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"First test");
+    //Hello
 }
 
 - (void)didReceiveMemoryWarning {
